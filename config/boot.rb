3da86d6ef3ed7e0d773de0ee441411c8055db726
事業システムG <jigyo-system@MAC036.local>
2016-07-05 18:48:34 +0900

    rails s -b 0.0.0.0 しなくても良いようにする(rails sだけでlocalhost:3000に繋がる)

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+# rails s -b 0.0.0.0 しなくても良いようにする(rails sだけでlocalhost:3000に繋がる)
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host:  '0.0.0.0', Port: 3000)
+    end
+  end
+end
